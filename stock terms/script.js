571a8a70a88239939d5fff33c6564ebd1751fb71
David <david.raoofz@gmail.com>
2026-05-05 04:17:12 -0700

    edit a part of stock terms

--- stock terms/script.js
+++ stock terms/script.js
@@ -7,3 +7,17 @@ items.forEach(item => {
         item.classList.toggle("active");
     });
 });
+
+const btn = document.getElementById("toggleBtn");
+
+btn.addEventListener("click", () => {
+  const hiddenItems = document.querySelectorAll(".item.hidden");
+
+  const isHidden = hiddenItems[0].style.display === "" || hiddenItems[0].style.display === "none";
+
+  hiddenItems.forEach(item => {
+    item.style.display = isHidden ? "block" : "none";
+  });
+
+  btn.textContent = isHidden ? "عرض أقل" : "عرض المزيد";
+});
